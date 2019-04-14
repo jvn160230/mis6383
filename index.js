49215e79a40fc2234a0db25bdf998f1528c69390
jvn160230 <49541053+jvn160230@users.noreply.github.com>
2019-04-13 20:08:38 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+var http = require ('http');
+var PORT = process.env.PORT || 5000;​
+​
+var express = require('express');​
+​
+var app = express();​
+​
+​
+​
+var http = require('http');​
+​
+var server = http.Server(app);​
+​
+​
+server.listen(PORT, function() {​
+​
+  console.log('Chat server running');​
+​
+});​
